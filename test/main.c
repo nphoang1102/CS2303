f218bf427977e9effdb18436f7155dfb80c81b16
Hoang Nguyen <nphoang1102@gmail.com>
2017-08-20 13:45:19 +0000

    testing with malloc and free

--- test/main.c
+++ test/main.c
@@ -1,8 +1,16 @@
 #include "hello.h"
 #include "name.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    print_hello();
-    input_name();
+    const int char_length = 256;
+    char *name = malloc(char_length);
+    printf("What is your name? ");
+    scanf("%255[^\n]s", name);
+    printf("F*ck you %s!\n", name);
+    free(name);
+    // print_hello();
+    // input_name();
     return 0;
 }
